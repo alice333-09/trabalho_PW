b37bc222b224277bac8c6f3867e35d895fa935b6
alice333-09 <alicelimadocarmo@outlook.com>
2026-06-07 17:40:49 +0000

    conexao e aqvs docker concluidos

--- conexao.php
+++ conexao.php
@@ -1,3 +1,3 @@
 <?php
-$conect= new PDO("mysql:host=localhost;dbname=trabalhocruds","root","");
+$conect = new PDO("mysql:host=db;dbname=trabalhocruds;charset=utf8", "root", "root");
 $conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if (!isset($_SESSION['usuario_id'])) {
         <main>
             <div class="grid grid-cols-1 gap-4 sm:grid-cols-3 sm:gap-6">
                 
-                <a href="CRUDFunc/indexfunc.php" class="group relative bg-white p-6 rounded-xl shadow-sm border border-gray-200 hover:shadow-md hover:border-blue-500 transition duration-200 ease-in-out flex flex-col justify-between">
+                <a href="CRUDfunc/indexfunc.php" class="group relative bg-white p-6 rounded-xl shadow-sm border border-gray-200 hover:shadow-md hover:border-blue-500 transition duration-200 ease-in-out flex flex-col justify-between">
                     <div>
                         <div class="w-10 h-10 bg-blue-50 rounded-lg flex items-center justify-center text-blue-600 group-hover:bg-blue-600 group-hover:text-white transition duration-200 mb-4">
                             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
